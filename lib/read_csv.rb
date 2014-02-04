e46dc7e90af4e2174bea7d6827d75f83c3e513db
Gavin Ching <gavinchingy@gmail.com>
2014-02-03 22:43:36 -0500

    Create directories and add Gemfile

--- lib/read_csv.rb
+++ lib/read_csv.rb
@@ -1,8 +1,10 @@
 ## csv_to_lead.rb
 ## Gavin Ching - February 3rd, 2014
 ## Description: Used to convert a given CSV file containing companies names and go step by step to fill in details for Podio integration.
+require 'CSV'
 
+CSV.foreach("YourNextStartup.csv") do |row|
+  puts "#{$.}" +" --- " + row[0] rescue nil
 
-CSV.foreach("YourNextStartup") do |row|
-  puts row
+  returns row
 end
---
+++ main.rb
@@ -0,0 +1,8 @@
+## main.rb
+## Gavin Ching - February 3rd, 2014
+## Description: This overall program helps with parsing a given CSV file and creates a CSV file that can be properly integrated with the current
+## sales tracker on Podio. (CR - AIESEC Toronto Personal)
+
+
+
+
